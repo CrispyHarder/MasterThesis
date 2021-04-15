7aa8f1222a37a98957157409e1d0d4ab1b366ca7
CrispyHarder <christian.harder96@gmail.com>
2021-04-15 15:25:52 +0200

    fixed bugs

--- ResNet/trainer.py
+++ ResNet/trainer.py
@@ -89,8 +89,8 @@ def main():
         # Check if the save_dir for the run exists or not,
         # path is save_dir/model_name(like resnet20)/run_ix
         save_dir_run = os.path.join(args.save_dir,args.arch,'run_{}'.format(nr_run))
-        if not os.path.exists(args.save_dir_run):
-            os.makedirs(args.save_dir_run)
+        if not os.path.exists(save_dir_run):
+            os.makedirs(save_dir_run)
         
         #add a writer to log training results for tensorboard
         writer = SummaryWriter(save_dir_run)
@@ -196,7 +196,7 @@ def main():
                 epochs_wo_improvement += 1 
 
             #if early breaking condition is met and we are after last lr change, we end the training
-            if epochs_wo_improvement >= breaking_condition and epochs > 150 + breaking_condition:
+            if epochs_wo_improvement >= breaking_condition and epoch > 150 + breaking_condition:
                 break
 
             if epoch > 0 and epoch % args.save_every == 0:
@@ -204,12 +204,12 @@ def main():
                     'epoch': epoch + 1,
                     'state_dict': model.state_dict(),
                     'best_prec1': best_prec1,
-                }, is_best,is_checkpoint = True, filename=os.path.join(args.save_dir_run, 'checkpoint.th'))
+                }, is_best,is_checkpoint = True, filename=os.path.join(save_dir_run, 'checkpoint.th'))
 
             save_checkpoint({
                 'state_dict': model.state_dict(),
                 'best_prec1': best_prec1,
-            }, is_best, is_checkpoint = False, filename=os.path.join(args.save_dir_run, 'model.th'))
+            }, is_best, is_checkpoint = False, filename=os.path.join(save_dir_run, 'model.th'))
         
         # empty the cache of the writer
         writer.flush()
