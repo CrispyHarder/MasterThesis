7d41a51e8299095695c6f884db303ac802bb3c3d
CrispyHarder <christian.harder96@gmail.com>
2021-04-21 16:14:44 +0200

    added a missing function weights_init

--- VQ_VAE/rk95_gh/PixelCNN.py
+++ VQ_VAE/rk95_gh/PixelCNN.py
@@ -4,6 +4,16 @@ import torch.nn.functional as F
 from torch.distributions.normal import Normal
 from torch.distributions import kl_divergence
 
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        try:
+            nn.init.xavier_uniform_(m.weight.data)
+            m.bias.data.fill_(0)
+        except AttributeError:
+            print("Skipping initialization of ", classname)
+            
 class GatedActivation(nn.Module):
     def __init__(self):
         super().__init__()
