9ccd18b7f4a18b7754639eddd22bdf95f3facd76
CrispyHarder <christian.harder96@gmail.com>
2021-05-03 20:22:26 +0200

    added a function for prediction_agreement; modified existing functions for task being either class or seg

--- util/parameter_comparison.py
+++ util/parameter_comparison.py
@@ -5,8 +5,10 @@ import torch.nn.functional as F
 import numpy as np 
 from ResNet import resnet # pylint: disable=import-error
 from util.saving import get_state_dict_from_checkpoint # pylint: disable=import-error
+from util.postprocessing import get_prediction # pylint: disable=import-error
+from util.average_meter import AverageMeter # pylint: disable=import-error
 
-def get_matrix_of_models(list_to_checkpoints, model_type, comparison_function):
+def get_matrix_of_models(list_to_checkpoints, model_type, comparison_function, **kwargs):
     '''compute a matrix for the different comparison values of comparison function
     Args:
         list_to_checkpoints(list(string)): a list of paths to the model checkpoints
@@ -28,7 +30,7 @@ def get_matrix_of_models(list_to_checkpoints, model_type, comparison_function):
     #compute the matrix
     for i in range(n_models):
         for j in range(i+1):
-            value = comparison_function(list_models[i],list_models[j])
+            value = comparison_function(list_models[i],list_models[j],**kwargs)
             matrix[i,j] = value 
             matrix[j,i] = value
     return matrix
@@ -47,3 +49,37 @@ def cosine_sim_model_params(model_0,model_1):
     params_1 = get_flattened_params(model_1)
     return F.cosine_similarity(params_0,params_1,dim=0)
 
+def prediction_agreement(model_0,model_1,dataloader,task):
+    '''computes the prediction disagreement for two models
+    Args:
+        model_0(nn.Module): One of the models to compare
+        model_1(nn.Module): The other model to compare 
+        dataloader(dataloader): The dataloader over which is iterated and on who the 
+            predictions are computed and compared
+        task(str): either 'class' or 'seg'. The task dictates
+            how exactly the disagreement is computed
+    
+    Returns(float): The prediction disagreement of the two models over the dataloader'''
+    agreement = AverageMeter()
+
+    for i,(img,_) in enumerate(dataloader):
+        if i > 10:
+            return agreement.avg
+        if task == 'class':
+            len_batch = len(img)
+
+            # get predictions
+            pred_0 = get_prediction(img,model_0,task)
+            pred_1 = get_prediction(img,model_1,task)
+
+            # get number of same predictions and then percentage
+            same_pred = pred_0.eq(pred_1).float().sum(0)
+            agree_percentage = same_pred/len_batch
+
+            #update the averageMeter
+            agreement.update(agree_percentage,len_batch)
+
+        if task == 'seg':
+            # prob compute dice score of both pred
+            raise NotImplementedError
+    return agreement.avg
