41ad9918d2ee7a1158a9fa769ca7cb414a0aa23b
CrispyHarder <christian.harder96@gmail.com>
2021-05-26 14:54:26 +0200

    added the vq layer and finished first version of model

--- VQ_VAE/resnet_cifar10/rnn_vq_vae.py
+++ VQ_VAE/resnet_cifar10/rnn_vq_vae.py
@@ -1,6 +1,76 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.distributions.normal import Normal
+from torch.distributions import kl, kl_divergence
+
+class VectorQuantizerEMA(nn.Module):
+    def __init__(self, num_embeddings, embedding_dim, commitment_cost, decay, epsilon=1e-5):
+        super(VectorQuantizerEMA, self).__init__()
+        
+        self._embedding_dim = embedding_dim
+        self._num_embeddings = num_embeddings
+        
+        self._embedding = nn.Embedding(self._num_embeddings, self._embedding_dim)
+        self._embedding.weight.data.normal_()
+        self._commitment_cost = commitment_cost
+        
+        self.register_buffer('_ema_cluster_size', torch.zeros(num_embeddings))
+        self._ema_w = nn.Parameter(torch.Tensor(num_embeddings, self._embedding_dim))
+        self._ema_w.data.normal_()
+        
+        self._decay = decay
+        self._epsilon = epsilon
+
+    def forward(self, inputs):
+        # convert inputs from BCHW -> BHWC
+        inputs = inputs.permute(0, 2, 3, 1).contiguous()
+        input_shape = inputs.shape
+        
+        # Flatten input
+        flat_input = inputs.view(-1, self._embedding_dim)
+        
+        # Calculate distances
+        distances = (torch.sum(flat_input**2, dim=1, keepdim=True) 
+                    + torch.sum(self._embedding.weight**2, dim=1)
+                    - 2 * torch.matmul(flat_input, self._embedding.weight.t()))
+            
+        # Encoding
+        encoding_indices = torch.argmin(distances, dim=1).unsqueeze(1)
+        encodings = torch.zeros(encoding_indices.shape[0], self._num_embeddings, device=inputs.device)
+        encodings.scatter_(1, encoding_indices, 1)
+        
+        # Quantize and unflatten
+        quantized = torch.matmul(encodings, self._embedding.weight).view(input_shape)
+        
+        # Use EMA to update the embedding vectors
+        if self.training:
+            self._ema_cluster_size = self._ema_cluster_size * self._decay + \
+                                     (1 - self._decay) * torch.sum(encodings, 0)
+            
+            # Laplace smoothing of the cluster size
+            n = torch.sum(self._ema_cluster_size.data)
+            self._ema_cluster_size = (
+                (self._ema_cluster_size + self._epsilon)
+                / (n + self._num_embeddings * self._epsilon) * n)
+            
+            dw = torch.matmul(encodings.t(), flat_input)
+            self._ema_w = nn.Parameter(self._ema_w * self._decay + (1 - self._decay) * dw)
+            
+            self._embedding.weight = nn.Parameter(self._ema_w / self._ema_cluster_size.unsqueeze(1))
+        
+        # Loss
+        e_latent_loss = F.mse_loss(quantized.detach(), inputs)
+        loss = self._commitment_cost * e_latent_loss
+        
+        # Straight Through Estimator
+        quantized = inputs + (quantized - inputs).detach()
+        avg_probs = torch.mean(encodings, dim=0)
+        perplexity = torch.exp(-torch.sum(avg_probs * torch.log(avg_probs + 1e-10)))
+        
+        # convert quantized from BHWC -> BCHW
+        return loss, quantized.permute(0, 3, 1, 2).contiguous(), perplexity, encodings
+
 
 class ConvRnnEncoder(nn.Module):
     def __init__(self, in_channels,hidden_dim,last_dim):
@@ -57,5 +127,31 @@ class ConvRnnDecoder(nn.Module):
         layers = torch.stack(layers)
         layers = layers.permute(1,0,2,3,4)
         return layers
+
+class VQConvRnnAE(nn.Module):
+    def __init__(self,in_channels,hidden_dim,last_dim,num_embeddings, 
+            embedding_dim, commitment_cost, decay):
+        super().__init__()
+        assert ( hidden_dim == embedding_dim)
+        self.encoder = ConvRnnEncoder(in_channels,hidden_dim,last_dim*2)
+        self.vq_vae = VectorQuantizerEMA(num_embeddings,embedding_dim,commitment_cost,decay)
+        self.decoder = ConvRnnDecoder(in_channels,hidden_dim,last_dim, number_layers=19)
+
+    def forward(self,x):
+        h1, h2 = self.encoder(x)
+
+        loss_vq, quantized, perplexity, _ = self.vq_vae(h1)
+
+        mu, logvar = h2.chunk(2, dim=1)
+        q_z_x = Normal(mu, logvar.mul(.5).exp())
+        p_z = Normal(torch.zeros_like(mu), torch.ones_like(logvar))
+        kl_div = kl_divergence(q_z_x, p_z).sum(1).mean()
+
+        h2_sample = q_z_x.rsample()
+
+        x_tilde = self.encoder(quantized,h2_sample)
+
+        return loss_vq, kl_div, x_tilde, perplexity
+
 
 
