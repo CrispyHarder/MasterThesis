04685395baa2c873788163259375f1d160c0c49a
CrispyHarder <christian.harder96@gmail.com>
2021-05-26 14:52:56 +0200

    added a file with scrips to move data in specific ways

---
+++ util/directory_scripts.py
@@ -0,0 +1,41 @@
+import os 
+import shutil
+import math
+from scipy.sparse import data
+from sklearn.model_selection import train_test_split
+
+def train_val_split(path_to_data,split):
+    '''A function that splits a directory into a train and a val directory
+    Args:
+        path_to_data(str): the path to the data to be split
+        split(float): the ratio of val data of whole data, setting to 
+            0 leads to no validation data'''
+    
+    # set paths and make directories
+    train_path = os.path.join(path_to_data,'train')
+    val_path = os.path.join(path_to_data,'val')
+    os.makedirs(train_path)
+    os.makedirs(val_path)
+
+    data_paths = os.listdir(path_to_data)
+    train_data,val_data=train_test_split(data_paths,test_size=split)
+
+    print(train_data)
+    for data_path in train_data:
+        data_path = os.path.join(path_to_data,data_path)
+        shutil.copy(data_path,train_path)
+
+    for data_path in val_data:
+        data_path = os.path.join(path_to_data,data_path)
+        shutil.copy(data_path,val_path)
+
+def rename_cp_to_model(path_to_data):
+    '''goes through the directory and renames checkpoint.th into model.th'''
+    for path in os.listdir(path_to_data):
+        path_to_model = os.path.join(path_to_data,path,'model.th')
+        path_to_cp = os.path.join(path_to_data,path,'checkpoint.th')
+        if not os.path.exists(path_to_model):
+            print(path_to_cp)
+            os.rename(path_to_cp,path_to_model)
+            
+
