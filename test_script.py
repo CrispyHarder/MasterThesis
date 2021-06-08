4474f4af70fa241b0ac6b96b50161de15091736b
CrispyHarder <christian.harder96@gmail.com>
2021-06-08 13:28:41 +0200

    added simple tests to test on devices

---
+++ test_script.py
@@ -0,0 +1,8 @@
+from tests.data import DataloaderTests, DatasetTests
+
+ds_tests = DatasetTests()
+ds_tests.test_all()
+
+dl_tests = DataloaderTests()
+dl_tests.test_all()
+
---
+++ tests/data.py
@@ -0,0 +1,64 @@
+import unittest
+import os 
+import torch
+from torch.utils.data.dataloader import DataLoader 
+
+class DatasetTests(unittest.TestCase):
+
+    def test_resnet_cifar10_parameters(self):
+        from data.datasets.resnet_cifar10_dataset import Resnet_cifar10_parameters_dataset # pylint: disable=all
+        path = os.path.join('storage','models','ResNet','cifar10')
+        ds = Resnet_cifar10_parameters_dataset(path,train=True) 
+        for i in range(ds.__len__()):
+            ds.__getitem__(i)
+
+        print('Resnet_cifar10_parameters working')
+
+    def test_resnet_cifar10_layer_parameters(self):
+        from data.datasets.resnet_cifar10_dataset import Resnet_cifar10_layer_parameters_dataset # pylint: disable=all
+        path = os.path.join('storage','models','ResNet','cifar10')
+        ds = Resnet_cifar10_layer_parameters_dataset(path,train=True) 
+        for i in range(ds.__len__()):
+            ds.__getitem__(i)
+
+        print('Resnet_cifar10_layer_parameters working')
+
+    def test_all(self):
+        self.test_resnet_cifar10_parameters()
+        self.test_resnet_cifar10_layer_parameters()
+
+class DataloaderTests(unittest.TestCase):
+
+    def __init__(self):
+        super().__init__()
+        from torch.utils.data import DataLoader
+        self.cuda_avail = torch.cuda.is_available()
+        self.num_workers = 4 if self.cuda_avail else 0
+
+    def test_resnet_cifar10_parameters(self):
+        from data.datasets.resnet_cifar10_dataset import Resnet_cifar10_parameters_dataset # pylint: disable=all
+        path = os.path.join('storage','models','ResNet','cifar10')
+        ds = Resnet_cifar10_parameters_dataset(path,train=True) 
+        dl = DataLoader(ds,2,num_workers=self.num_workers)
+        for output in enumerate(dl):
+            pass 
+        print('test_resnet_cifar10_parameters dataloader working')
+
+    def test_resnet_cifar10_layer_parameters(self):
+        from data.datasets.resnet_cifar10_dataset import Resnet_cifar10_layer_parameters_dataset # pylint: disable=all
+        path = os.path.join('storage','models','ResNet','cifar10')
+        ds = Resnet_cifar10_layer_parameters_dataset(path,train=True) 
+        dl = DataLoader(ds,2,num_workers=self.num_workers)
+        for output in enumerate(dl):
+            pass 
+        print('test_resnet_cifar10_layer_parameters dataloader working')
+    
+    def test_all(self):
+        self.test_resnet_cifar10_parameters()
+        self.test_resnet_cifar10_layer_parameters()
+
+
+
+
+
+        
