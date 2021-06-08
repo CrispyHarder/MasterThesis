aaa116e10eded3d14aefcba40fe49085613d8e9c
CrispyHarder <christian.harder96@gmail.com>
2021-06-08 14:09:17 +0200

    added a pin memory and shuffle, as well as a torch.multiprocessing.set_start_method('spawn') in data tests for the dataloader

--- tests/data.py
+++ tests/data.py
@@ -34,12 +34,14 @@ class DataloaderTests(unittest.TestCase):
         from torch.utils.data import DataLoader
         self.cuda_avail = torch.cuda.is_available()
         self.num_workers = 4 if self.cuda_avail else 0
+        if self.cuda_avail:
+            torch.multiprocessing.set_start_method('spawn')
 
     def test_resnet_cifar10_parameters(self):
         from data.datasets.resnet_cifar10_dataset import Resnet_cifar10_parameters_dataset # pylint: disable=all
         path = os.path.join('storage','models','ResNet','cifar10')
         ds = Resnet_cifar10_parameters_dataset(path,train=True) 
-        dl = DataLoader(ds,2,num_workers=self.num_workers)
+        dl = DataLoader(ds,2,num_workers=self.num_workers,pin_memory=True,shuffle=True)
         for output in enumerate(dl):
             pass 
         print('test_resnet_cifar10_parameters dataloader working')
@@ -48,7 +50,7 @@ class DataloaderTests(unittest.TestCase):
         from data.datasets.resnet_cifar10_dataset import Resnet_cifar10_layer_parameters_dataset # pylint: disable=all
         path = os.path.join('storage','models','ResNet','cifar10')
         ds = Resnet_cifar10_layer_parameters_dataset(path,train=True) 
-        dl = DataLoader(ds,2,num_workers=self.num_workers)
+        dl = DataLoader(ds,2,num_workers=self.num_workers,pin_memory=True,shuffle=True)
         for output in enumerate(dl):
             pass 
         print('test_resnet_cifar10_layer_parameters dataloader working')
