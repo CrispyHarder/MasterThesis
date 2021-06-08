667daa306b0ecae8afbd9b13c5eaa2413689df29
CrispyHarder <christian.harder96@gmail.com>
2021-06-08 14:20:31 +0200

    added a custom test loop in test csript, where only dl for layer ds (baseline usage) is testet

--- test_script.py
+++ test_script.py
@@ -5,10 +5,15 @@ parser = argparse.ArgumentParser(description='argparser for tests')
 parser.add_argument('--datasets',default=False, action='store_true')
 parser.add_argument('--dataloader',default=False, action='store_true')
 parser.add_argument('-all',default=False, action='store_true')
+parser.add_argument('--spec',default=False, action='store_true')
 
 def main():
     global args
     args = parser.parse_args()
+    if args.spec:
+        dl_tests = DataloaderTests()
+        dl_tests.test_resnet_cifar10_layer_parameters()
+        return
     if args.all:
         ds_tests = DatasetTests()
         ds_tests.test_all()
